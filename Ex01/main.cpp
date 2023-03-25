b4bc1f2e0d4c98482a1af8ef4999d574f719050d
Leo Azevedo <leozinnnn3@gmail.com>
2023-03-25 16:49:09 -0300

    Finalizadno Ex 1

--- Ex01/main.cpp
+++ Ex01/main.cpp
@@ -2,8 +2,21 @@
 
 using namespace std;
 
+int fatorial(int n)
+{
+    if(n == 0)
+        return 1;
+
+    return n*fatorial(n-1);
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    int numero, result;
+    cout << "Digite um numero: ";
+    cin >> numero;
+
+    result = fatorial(numero);
+    cout << "O Resultado do Fatorial e : " << result << endl;
     return 0;
 }
